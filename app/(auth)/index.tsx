aea4c87d35bbbf76cae2ca656422bd9b9b1ae45f
Muhammad Haris <iamharisdev@gmail.com>
2025-08-19 16:21:42 +0500

    remove buttons on login screen

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -11,11 +11,7 @@ const Login = () => {
     <View>
       <Text>Login</Text>
       <Button title="Login" onPress={() => dispatch(setToken("demo-token"))} />
-      <Button title="Go to Signup" onPress={() => router.push("/signup")} />
-      <Button
-        title="Forgot Password?"
-        onPress={() => router.push("/forgot")}
-      />
+    
     </View>
   );
 };
